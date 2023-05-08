9ffd0ffa6f041b3533bf07711a2acfb1b121acb8
Flavio Monteiro <flaviomonteiro2013@gmail.com>
2023-05-08 04:08:27 -0300

    Iniciar projeto da calculadora
    npm init vite@latest e algumas modificações

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import Header from "./components/Header"
+
+function App() {
+  return (
+    <>
+      <Header /> 
+      {/* <Calculadora />  */}
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+const Header = ({titulo}) => {
+    return (
+        <div class="container bg-light">
+            <h1 className="p-3 text-center">Calculadora de IMC</h1>
+        </div>
+    )
+}
+
+export default Header
